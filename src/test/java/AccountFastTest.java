f6b605b9b6433d080dc983b66e1119774e78e170
anhtv08 <anhtv010888@gmail.com>
2019-03-29 08:07:35 +0800

    clean up

--- src/test/java/AccountFastTest.java
+++ src/test/java/AccountFastTest.java
@@ -1,16 +1,12 @@
 import category.UnitTest;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
 import static org.testng.Assert.assertEquals;
-
 @UnitTest
 class AccountFastTest {
 
     Account account;
     @Test
-    @Tag("fast")
     public void quickTest(){
         account= new Account(100);
 
--- src/test/java/AccountSlowTest.java
+++ src/test/java/AccountSlowTest.java
@@ -1,6 +1,5 @@
 
 import category.UatTest;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.testng.Assert;
 @UatTest
@@ -9,7 +8,6 @@ class AccountSlowTest {
     Account account;
 
     @Test
-    @Tag("slow")
     public void slowTest() throws InterruptedException {
         account= new Account(100);
         System.out.println("in slow test");
